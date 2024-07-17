49fb77867937d0ea6c3b1f3fe8d7d2dc2cdd83ab
fazil2915 <fazilrah9846@gmail.com>
2024-07-18 01:02:53 +0530

    resolved bug

--- client/src/scenes/widgets/PostsWidget.jsx
+++ client/src/scenes/widgets/PostsWidget.jsx
@@ -6,24 +6,34 @@ import PostWidget from "./PostWidget"
 const PostsWidget = ({ userId, isProfile=false }) => {
     const dispatch = useDispatch();
     const posts = useSelector((state) => state.posts)
+    
     const token = useSelector((state) => state.token);
 
     const getPosts = async () => {
+      try{
         const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/posts`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
         const data = await response.json();
         dispatch(setPosts({ posts: data }))
+      }
+      catch(error){
+        console.log("failed to load getPosts",error);
+      }
     }
 
     const getUserPosts = async () => {
+      try{
         const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/posts/${userId}/post`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
         const data = await response.json();
-        dispatch(setPosts({ posts: data }))
+        dispatch(setPosts({ posts: data}))
+      }catch(error){
+        console.log("failed to load getUserPosts",error);
+      }
     }
     useEffect(() => {
         if (isProfile) {
@@ -35,7 +45,7 @@ const PostsWidget = ({ userId, isProfile=false }) => {
 
     return (
         <>
-          {posts.map(
+          {Array.isArray(posts)&& posts.map(
             ({
               _id,
               userId,
